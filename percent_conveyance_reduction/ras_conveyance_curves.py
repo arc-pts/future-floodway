f23c66333029b77fd27a4870733c8167019f4a06
Jacob Bates <jacob.bates@wsp.com>
2024-09-25 17:11:53 -0500

    handle bugs in ras hdf curves, improvements

--- percent_conveyance_reduction/ras_conveyance_curves.py
+++ percent_conveyance_reduction/ras_conveyance_curves.py
@@ -3,8 +3,12 @@ import pandas as pd
 from rashdf import RasGeomHdf
 import numpy as np
 import geopandas as gpd
+from typing import Union
 
-def get_conveyance_and_mannings_curves(geom_hdf: PathLike) -> pd.DataFrame:
+def get_conveyance_and_mannings_curves(
+    geom_hdf: PathLike,
+    to_dataframe: bool = False
+) -> Union[pd.DataFrame, dict]:
     with RasGeomHdf(geom_hdf) as ghdf:
         mesh_names = ghdf.mesh_area_names()
         face_curves = {
@@ -16,21 +20,34 @@ def get_conveyance_and_mannings_curves(geom_hdf: PathLike) -> pd.DataFrame:
             "wetted_perimeter": [],
             "hydraulic_radius": [],
             "conveyance": []
-        }
+        } if to_dataframe else {}
         for name in mesh_names:
             FACES_AREA_ELEV_INFO = ghdf[rf"/Geometry/2D Flow Areas/{name}/Faces Area Elevation Info"][()]
             FACES_AREA_ELEV_VALUES = ghdf[rf"/Geometry/2D Flow Areas/{name}/Faces Area Elevation Values"][()]
-            face_curves["mesh_name"] += [name] * FACES_AREA_ELEV_VALUES.shape[0]
-            for face_id, (start, cnt) in enumerate(FACES_AREA_ELEV_INFO):
-                face_curves["face_id"] += [face_id] * cnt
-                elev, area, wet_perim, mann_n = FACES_AREA_ELEV_VALUES[start:start+cnt].T
-                face_curves["elevation"] += list(elev)
-                face_curves["mannings_n"] += list(mann_n)
-                face_curves["area"] += list(area)
-                face_curves["wetted_perimeter"] += list(wet_perim)
-                face_curves["hydraulic_radius"] += list(area / wet_perim)
-                face_curves["conveyance"] += list(1.486 / mann_n * (area * ((area / wet_perim) ** (2 / 3))))
-        return pd.DataFrame(face_curves).fillna(0)
+            if to_dataframe:
+                face_curves["mesh_name"] += [name] * FACES_AREA_ELEV_VALUES.shape[0]
+                for face_id, (start, cnt) in enumerate(FACES_AREA_ELEV_INFO):
+                    face_curves["face_id"] += [face_id] * cnt
+                    elev, area, wet_perim, mann_n = FACES_AREA_ELEV_VALUES[start:start+cnt].T
+                    face_curves["elevation"] += list(elev)
+                    face_curves["mannings_n"] += list(mann_n)
+                    face_curves["area"] += list(area)
+                    face_curves["wetted_perimeter"] += list(wet_perim)
+                    face_curves["hydraulic_radius"] += list(np.nan_to_num(area / wet_perim))
+                    face_curves["conveyance"] += list(np.nan_to_num(1.486 / mann_n * (area * ((area / wet_perim) ** (2 / 3)))))
+            else:
+                face_curves[name] = {}
+                for face_id, (start, cnt) in enumerate(FACES_AREA_ELEV_INFO):
+                    elev, area, wet_perim, mann_n = FACES_AREA_ELEV_VALUES[start:start+cnt].T
+                    face_curves[name][face_id] = dict(
+                        elevation = elev,
+                        mannings_n = mann_n,
+                        area = area,
+                        wetted_perimeter = wet_perim,
+                        hydraulic_radius = np.nan_to_num(area / wet_perim),
+                        conveyance = np.nan_to_num(1.486 / mann_n * (area * ((area / wet_perim) ** (2 / 3))))
+                    )
+        return pd.DataFrame(face_curves) if to_dataframe else face_curves
     
 def evaluate_conveyance_reduction(
     initial_geom_hdf: PathLike,
@@ -41,30 +58,32 @@ def evaluate_conveyance_reduction(
         assert mesh_faces.to_json() == ghdf2.mesh_cell_faces().to_json()
     df1 = get_conveyance_and_mannings_curves(initial_geom_hdf)
     df2 = get_conveyance_and_mannings_curves(updated_geom_hdf)
-    assert df1["elevation"].to_list() == df2["elevation"].to_list()
-    mesh_faces["delta_conveyance"] = mesh_faces["face_id"].apply(
-        lambda face_id: 1 - (
-            np.trapz(
-                y=df2.loc[df2["face_id"] == face_id]["conveyance"].to_numpy(),
-                x=df2.loc[df2["face_id"] == face_id]["elevation"].to_numpy()
-            ) / np.trapz(
-                y=df1.loc[df1["face_id"] == face_id]["conveyance"].to_numpy(),
-                x=df1.loc[df1["face_id"] == face_id]["elevation"].to_numpy()
-            )
-        )
+    mesh_faces["delta_q_perc"] = mesh_faces.apply(
+        lambda row: round(
+            (
+                np.median(df2[row["mesh_name"]][row["face_id"]]["conveyance"]) /
+                np.median(df1[row["mesh_name"]][row["face_id"]]["conveyance"])
+            ) - 1,
+            2
+        ) * 100,
+        axis = 1
     )
-    mesh_faces["delta_mann_n"] = mesh_faces["face_id"].apply(
-        lambda face_id: 1 - (
-            np.trapz(
-                y=df2.loc[df2["face_id"] == face_id]["mannings_n"].to_numpy(),
-                x=df2.loc[df2["face_id"] == face_id]["elevation"].to_numpy()
-            ) / np.trapz(
-                y=df1.loc[df1["face_id"] == face_id]["mannings_n"].to_numpy(),
-                x=df1.loc[df1["face_id"] == face_id]["elevation"].to_numpy()
-            )
-        )
+    mesh_faces["delta_n_perc"] = mesh_faces.apply(
+        lambda row: round(
+            (
+                df2[row["mesh_name"]][row["face_id"]]["mannings_n"][0] /
+                df1[row["mesh_name"]][row["face_id"]]["mannings_n"][0]
+            ) - 1,
+            2
+        ) * 100,
+        axis = 1
     )
     return mesh_faces
 
 if __name__ == "__main__":
-    pass
+    gdf = evaluate_conveyance_reduction(
+        r"C:\Users\USJB713989\Downloads\OneDrive_2024-09-25\Briar Creek\Base Geometry\Briar_Creek_WS.g02.hdf",
+        r"C:\Users\USJB713989\Downloads\OneDrive_2024-09-25\Briar Creek\H1 to H5 Nval 10%  Reduction\Briar_Creek_WS.g03.hdf"
+    )
+    print(gdf)
+    gdf.to_file(r"C:\Users\USJB713989\Downloads\OneDrive_2024-09-25\Briar Creek\conv.shp")
